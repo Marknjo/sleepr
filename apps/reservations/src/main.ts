5030cc55d0993614f1afd47ceea5d2b0b6ecb3ca
Mark Njoroge <marknjo7@gmail.com>
2023-01-09 11:57:08 +0300

    feat: setup global validations with ValidatationPipe

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { ReservationsModule } from './reservations.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule)
+
+  // validations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  )
+
   await app.listen(3000)
 }
 bootstrap()
