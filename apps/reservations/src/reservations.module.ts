5a1246f69f342855822bd5b5654063e5a1e1907c
Mark Njoroge <marknjo7@gmail.com>
2023-01-09 11:59:57 +0300

    feat: import DatabaseModule & Reservation schema

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common'
 import { ReservationsService } from './reservations.service'
 import { ReservationsController } from './reservations.controller'
 import { DatabaseModule } from '@app/common'
+import { ReservationsRepository } from './reservations.repository'
+import { Reservation, ReservationSchema } from './schemas/reservation.schema'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeatureAsync([
+      {
+        name: Reservation.name,
+        useFactory: () => {
+          const schema = ReservationSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
