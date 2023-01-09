c3e163c139e71545d1c52a52f1a20ec335c879bf
Mark Njoroge <marknjo7@gmail.com>
2023-01-09 11:58:55 +0300

    feat: setup pino Logger

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { ReservationsModule } from './reservations.module'
 import { ValidationPipe } from '@nestjs/common'
+import { Logger } from 'nestjs-pino'
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule)
@@ -17,6 +18,9 @@ async function bootstrap() {
       },
     }),
   )
+
+  // logger
+  app.useLogger(app.get(Logger))
 
   await app.listen(3000)
 }
