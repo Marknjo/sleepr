045c0d1be09d083e8ecd22d909d9ae711c8f1a44
Mark Njoroge <marknjo7@gmail.com>
2023-01-10 08:16:09 +0300

    feat(Reservations): implement ReservationShema details

--- apps/reservations/src/schemas/reservation.schema.ts
+++ apps/reservations/src/schemas/reservation.schema.ts
@@ -1,1 +1,30 @@
-export class Reservation {}
+import { Abstract } from '@app/common'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+@Schema({
+  versionKey: false,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class Reservation extends Abstract {
+  @Prop()
+  startDate: Date
+
+  @Prop()
+  endDate: Date
+
+  @Prop()
+  userId: string
+
+  @Prop()
+  placeId: string
+
+  @Prop()
+  invoiceId: string
+}
+
+export const ReservationSchema = SchemaFactory.createForClass(Reservation)
+
+export type TReservationDoc = HydratedDocument<Reservation>
