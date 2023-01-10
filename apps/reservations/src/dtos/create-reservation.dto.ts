a1df07a2a2a04e71d5a1927e18974a345a8a942f
Mark Njoroge <marknjo7@gmail.com>
2023-01-10 08:16:31 +0300

    feat(Reservations): implement CreateReservastionDto

--- apps/reservations/src/dtos/create-reservation.dto.ts
+++ apps/reservations/src/dtos/create-reservation.dto.ts
@@ -1,1 +1,18 @@
-export class CreateReservationDto {}
+import { Type } from 'class-transformer'
+import { IsDate, IsMongoId } from 'class-validator'
+
+export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date
+
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date
+
+  @IsMongoId()
+  placeId: string
+
+  @IsMongoId()
+  invoiceId: string
+}
