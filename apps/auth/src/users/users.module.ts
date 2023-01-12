eca78030288c51d768e84f57a51f52111e5dae4c
Mark Njoroge <marknjo7@gmail.com>
2023-01-12 09:52:10 +0300

    ci(auth): add DatabaseModule and import UserSchema

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
+import { UserRepository } from './user.repository'
+import { DatabaseModule } from '@app/common'
+import { User, UserSchema } from './schemas/user.schema'
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserRepository],
 })
 export class UsersModule {}
