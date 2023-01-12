b2f80cb4aaced212666241450be5b98c49530ef1
Mark Njoroge <marknjo7@gmail.com>
2023-01-12 09:48:48 +0300

    feat(auth): implement users schema

--- apps/auth/src/users/schemas/user.schema.ts
+++ apps/auth/src/users/schemas/user.schema.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Abstract } from '@app/common'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+@Schema({
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
+  timestamps: true,
+  versionKey: false,
+})
+export class User extends Abstract {
+  @Prop()
+  email: string
+
+  @Prop()
+  password: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
+
+export type TUserDoc = HydratedDocument<User>
