e58c3c1f7ae3ce327340832475166649ab027351
Mark Njoroge <marknjo7@gmail.com>
2023-01-12 09:52:51 +0300

    feat(auth/UsersService): implement create() method

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { UserRepository } from './user.repository'
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly userRepo: UserRepository) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user'
+    return this.userRepo.create(createUserDto)
   }
 
   findAll() {
