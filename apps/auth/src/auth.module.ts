0e2459cf8f038c93e7150e7ffc83c8c8f180b8c7
Mark Njoroge <marknjo7@gmail.com>
2023-01-14 10:34:28 +0300

    ci(auth): import PinoLoggerModule to the authDefault module

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { UsersModule } from './users/users.module'
+import { PinoLoggerModule } from '@app/common'
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PinoLoggerModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
