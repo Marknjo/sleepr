b227f031813909ade4c48deb2f9873f57751e060
Mark Njoroge <marknjo7@gmail.com>
2023-01-14 10:33:52 +0300

    ci: add PinoLogger & global ValidationPipe with default configs

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core'
 import { AuthModule } from './auth.module'
+import { ValidationPipe } from '@nestjs/common'
+import { Logger } from 'nestjs-pino'
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule)
+
+  // add validations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+      transformOptions: {
+        excludeExtraneousValues: true,
+      },
+    }),
+  )
+
+  // add pino logger
+
+  app.useLogger(app.get(Logger))
+
   await app.listen(3000)
 }
 bootstrap()
