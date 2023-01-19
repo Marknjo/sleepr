936045ec6b9089ebbd0d6b2dce12bffcd0029318
Mark Njoroge <marknjo7@gmail.com>
2023-01-19 08:12:09 +0300

    feat(Auth/AuthController): implement a signIn route handler

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Post, Res, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service'
 
-@Controller()
+import { ActiveUser } from './decorators/active-user.decorator'
+import { User } from './users/schemas/user.schema'
+import { Response } from 'express'
+
+import { LocalAuthGuard } from './guards/local-auth.guard'
+
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello()
+  @UseGuards(LocalAuthGuard)
+  @Post('sign-in')
+  async signIn(
+    @ActiveUser() activeUserData: User,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    await this.authService.signIn(activeUserData, res)
+
+    res.send(activeUserData)
   }
 }
