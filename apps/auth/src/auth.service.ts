6e9f494d32d7380a5a8fbad83cbaa5fa7b8cdacc
Mark Njoroge <marknjo7@gmail.com>
2023-01-19 08:30:11 +0300

    feat(AuthService): implement signUp

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -5,6 +5,8 @@ import { ConfigType } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 import { jwtConfigs } from './configs/jwt-configs'
 import { TokenPayload } from './interfaces/token-payload.interface'
+import { UsersService } from './users/users.service'
+import { CreateUserDto } from './users/dto/create-user.dto'
 
 @Injectable()
 export class AuthService {
@@ -13,6 +15,8 @@ export class AuthService {
     private readonly jwtConfig: ConfigType<typeof jwtConfigs>,
 
     private readonly jwtService: JwtService,
+
+    private readonly usersService: UsersService,
   ) {}
 
   async signIn(activeUserData: User, res: Response) {
@@ -36,5 +40,19 @@ export class AuthService {
     })
 
     return token
+  }
+
+  async signUp(createUserDto: CreateUserDto, res: Response) {
+    const user = await this.usersService.create(createUserDto)
+
+    await this.signIn(user, res)
+
+    user['id'] = user._id
+
+    // prune unused fields // @TODO: implement interceptor for the job
+    delete user['password']
+    delete user['_id']
+
+    return user
   }
 }
