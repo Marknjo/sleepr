950d23a8f74f9cad892847cc40e9ee54d8ecc1e4
Mark Njoroge <marknjo7@gmail.com>
2023-01-19 08:04:13 +0300

    feat(Auth/AuthService): implement a signIn() method to authenticate a user

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+import { User } from './users/schemas/user.schema'
+import { Response } from 'express'
+import { ConfigType } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { jwtConfigs } from './configs/jwt-configs'
+import { TokenPayload } from './interfaces/token-payload.interface'
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!'
+  constructor(
+    @Inject(jwtConfigs.KEY)
+    private readonly jwtConfig: ConfigType<typeof jwtConfigs>,
+
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signIn(activeUserData: User, res: Response) {
+    const tokenPayload: TokenPayload = {
+      userId: activeUserData._id.toHexString(),
+    }
+
+    const expires = new Date()
+    expires.setSeconds(expires.getSeconds() * this.jwtConfig.accessTokenTtl)
+
+    const token = await this.jwtService.signAsync(tokenPayload, {
+      expiresIn: this.jwtConfig.accessTokenTtl,
+      secret: this.jwtConfig.secret,
+      issuer: this.jwtConfig.issuer,
+      audience: this.jwtConfig.audience,
+    })
+
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+    })
+
+    return token
   }
 }
