887a91c7644195b25a40209f55805ce278e4bd61
Mark Njoroge <marknjo7@gmail.com>
2023-01-26 11:08:13 +0300

    ci(auth): setup microservices with tcp connection

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,11 +4,26 @@ import { Logger, ValidationPipe } from '@nestjs/common'
 import { Logger as PinoLogger } from 'nestjs-pino'
 import { ConfigService } from '@nestjs/config'
 import * as cookieParser from 'cookie-parser'
+import { Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule)
 
+  // configs
+  const configService = app.get(ConfigService)
+
   app.use(cookieParser())
+
+  // setup microservices
+
+  const tcpPort = configService.get('TCP_PORT')
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: tcpPort,
+    },
+  })
 
   // add validations
   app.useGlobalPipes(
@@ -27,12 +42,15 @@ async function bootstrap() {
   app.useLogger(app.get(PinoLogger))
 
   // start app
-  const configService = app.get(ConfigService)
-  const port = configService.get('API_PORT')
-  await app.listen(port)
+
+  const apiPort = configService.get('API_PORT')
+  await app.listen(apiPort)
+
+  // start microservices
+  await app.startAllMicroservices()
 
   // log api default url
   const logger = new Logger('Auth API', { timestamp: true })
-  logger.log(`App running on http://localhost:${port}`)
+  logger.log(`App running on http://localhost:${tcpPort}`)
 }
 bootstrap()
