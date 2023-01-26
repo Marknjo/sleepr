37f9d85640c54c58f5085fe8ece53ba9a0f4a58d
Mark Njoroge <marknjo7@gmail.com>
2023-01-26 09:43:21 +0300

    feat(payments): implement createCharge method to accept payments

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, LoggerService } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import Stripe from 'stripe'
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!'
+  private readonly logger: LoggerService = new Logger(PaymentsService.name)
+
+  private readonly stripe = new Stripe(
+    this.configSrv.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2023-10-16',
+    },
+  )
+
+  constructor(private readonly configSrv: ConfigService) {}
+
+  async createCharge(
+    card: Stripe.PaymentMethodCreateParams.Card1,
+    amount: number,
+  ) {
+    // create payment method
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    })
+
+    // create payment intent
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'used',
+    })
+
+    this.logger.log('Created a new payment intent')
+
+    return paymentIntent
   }
 }
