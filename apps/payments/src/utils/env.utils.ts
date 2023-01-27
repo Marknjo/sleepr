ec4cdd5a04f8e01f1c0517f57f7b6f9cd8b85819
Mark Njoroge <marknjo7@gmail.com>
2023-01-27 09:06:51 +0300

    ci(payments/validationSchema): require NODE_ENV variable to be always provided for

--- apps/payments/src/utils/env.utils.ts
+++ apps/payments/src/utils/env.utils.ts
@@ -4,6 +4,7 @@ const validationSchema = () =>
   Joi.object({
     // APP
     API_PORT: Joi.number().default(3000),
+    NODE_ENV: Joi.string().required(),
     TCP_PORT: Joi.number().required(),
     TCP_HOST: Joi.string().required(),
     STRIPE_SECRET_KEY: Joi.string().required(),
